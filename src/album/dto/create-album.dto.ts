9f416c7ffb29c0894f082002c711a5f0d00ec79c
luis <luisvaleroc22@gmail.com>
2023-10-27 23:25:16 -0300

    configuracion mongodb

---
+++ src/album/dto/create-album.dto.ts
@@ -0,0 +1,5 @@
+export class CreateAlbumDto {
+    name: string;
+    image: string;
+    artista: string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1/27017')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
