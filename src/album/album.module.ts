58f3ebf12efd3c020e12e2548d9f006b5456503b
luisvaleroc <luisvaleroc22@gmail.com>
2023-10-29 15:07:11 -0300

    mayuscula

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AlbumSchema } from './schemas/Album.schema';
+import { AlbumSchema } from './schemas/album.schema';
 import { Album } from './entities/album.entity';
 
 @Module({
