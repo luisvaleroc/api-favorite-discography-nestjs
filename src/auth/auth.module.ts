f0d49aa9451fdd472a9433fa4aeda58e8a7196ed
luisvaleroc <luisvaleroc22@gmail.com>
2023-11-02 23:20:30 -0300

    se implementa clave secreta enel .env jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports:[UsersModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
       global: true,
-      secret: "token_prueba",
+      secret: configService.get<string>('JWT_SECRET'),
       signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
     })],
   controllers: [AuthController],
   providers: [AuthService]
